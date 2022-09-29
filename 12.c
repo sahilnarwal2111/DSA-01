4c554862d5eb7bbde2a4bd33bf2bd7b415c35037
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-09-29 09:19:46 +0530

    updated

--- 12.c
+++ 12.c
@@ -35,6 +35,6 @@ int main()
     int arr[] = {1,2,3,4,5,6,7,8,9};
     int size = sizeof(arr)/sizeof(int);
     binarySearch(arr,5,size);
-    // linearSearch(arr,4,size);
+    // linearSearch(arr,4,size); time comlplexity is O(n)
     return 0;
 }
