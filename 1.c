4a0bafe95eb81b48f24ebea6f20f797e07ef51bf
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-09-30 09:53:10 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -3,6 +3,9 @@
 int main()
 {
     printf("Hello World");
+    printf("Hello World");printf("Hello World");
+    printf("Hello World");printf("Hello World");
+    printf("Hello World");printf("Hello World");
     printf("Hello World");
     return 0
 }
