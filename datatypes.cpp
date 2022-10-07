b190dba78942c23f451c4f5d18792f6dd0424d29
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-07 23:17:35 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a; //declaration
+    a = 12; //intializatioñ
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of character "<<sizeof(c)<<endl;
+
+    bool d;
+    cout<<"size of bool "<<sizeof(d)<<endl;
+
+    short int si;
+    long int li;
+}
