91a6ee143233ab43d963d148cb28f29e31e9bc3a
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-07 23:16:32 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float n1,n2;
+    cin>>n1>>n2;
+    char op;
+    cout<<"input an operator";
+    cin>>op;
+    switch (op)
+    {
+    case '+':
+        cout<< n1 + n2 <<endl;
+        break;
+    case '-':
+        cout<< n1 - n2 <<endl;
+        break;
+    case '*':
+        cout<< n1 * n2 <<endl;
+        break;
+    case '/':
+        cout<< n1 / n2 <<endl;
+        break;
+    
+    default:
+        cout<< "Enter another operator"<<endl;
+        break;
+    }
+    return 0;
+}
