6bd25c7b9d4a86589563dc971e7decab0718c631
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-10 23:09:26 +0530

    day-2 cp

---
+++ bitwise_operator.cpp
@@ -0,0 +1,35 @@
+//AND operator
+//OR operator
+//XOR operator:
+    //if both value are same ->0
+    //if both value are different ->1
+//ONES complement operator:
+    // 0->1 and 1->0
+
+//Left shift operator:
+    /*
+    left shift operator
+    4<<1
+    0100 -> 1000
+    a<<n = a*2^n
+    it shifts the all the bits to left 
+    right shift operator
+    4>>1
+    0100 ->0010
+    a>>n = a/2^n
+    it shifts the all bits to right
+    */ 
+
+// a+=b -> a = a + b
+// a-=b -> a = a - b
+
+//ternary operator
+// condition?X:Y
+//X when true and Y when false
+
+
+//Comma operator
+/*
+a = (2,3,4)
+a = 4
+*/
