6b7d5eb8adefaace322236d38fe24b61e2582fcf
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-12 23:38:02 +0530

    fcfs_java

--- fcfs.java
+++ fcfs.java
@@ -1,72 +1,76 @@
-// C program for implementation of FCFS
+// Java program for implementation of FCFS
 // scheduling
-#include<stdio.h>
-// Function to find the waiting time for all
-// processes
-void findWaitingTime(int processes[], int n,
-						int bt[], int wt[])
-{
-	// waiting time for first process is 0
-	wt[0] = 0;
 
-	// calculating waiting time
-	for (int i = 1; i < n ; i++ )
-		wt[i] = bt[i-1] + wt[i-1] ;
-}
+import java.text.ParseException;
 
-// Function to calculate turn around time
-void findTurnAroundTime( int processes[], int n,
-				int bt[], int wt[], int tat[])
-{
-	// calculating turnaround time by adding
-	// bt[i] + wt[i]
-	for (int i = 0; i < n ; i++)
-		tat[i] = bt[i] + wt[i];
-}
+class GFG {
 
-//Function to calculate average time
-void findavgTime( int processes[], int n, int bt[])
-{
-	int wt[n], tat[n], total_wt = 0, total_tat = 0;
+	// Function to find the waiting time for all
+	// processes
+	static void findWaitingTime(int processes[], int n,
+			int bt[], int wt[]) {
+		// waiting time for first process is 0
+		wt[0] = 0;
 
-	//Function to find waiting time of all processes
-	findWaitingTime(processes, n, bt, wt);
-
-	//Function to find turn around time for all processes
-	findTurnAroundTime(processes, n, bt, wt, tat);
-
-	//Display processes along with all details
-	printf("Processes Burst time Waiting time Turn around time\n");
-
-	// Calculate total waiting time and total turn
-	// around time
-	for (int i=0; i<n; i++)
-	{
-		total_wt = total_wt + wt[i];
-		total_tat = total_tat + tat[i];
-		printf(" %d ",(i+1));
-		printf("	 %d ", bt[i] );
-		printf("	 %d",wt[i] );
-		printf("	 %d\n",tat[i] );
+		// calculating waiting time
+		for (int i = 1; i < n; i++) {
+			wt[i] = bt[i - 1] + wt[i - 1];
+		}
 	}
-	int s=(float)total_wt / (float)n;
-	int t=(float)total_tat / (float)n;
-	printf("Average waiting time = %d",s);
-	printf("\n");
-	printf("Average turn around time = %d ",t);
+
+	// Function to calculate turn around time
+	static void findTurnAroundTime(int processes[], int n,
+			int bt[], int wt[], int tat[]) {
+		// calculating turnaround time by adding
+		// bt[i] + wt[i]
+		for (int i = 0; i < n; i++) {
+			tat[i] = bt[i] + wt[i];
+		}
+	}
+
+	//Function to calculate average time
+	static void findavgTime(int processes[], int n, int bt[]) {
+		int wt[] = new int[n], tat[] = new int[n];
+		int total_wt = 0, total_tat = 0;
+
+		//Function to find waiting time of all processes
+		findWaitingTime(processes, n, bt, wt);
+
+		//Function to find turn around time for all processes
+		findTurnAroundTime(processes, n, bt, wt, tat);
+
+		//Display processes along with all details
+		System.out.printf("Processes Burst time Waiting"
+					+" time Turn around time\n");
+
+		// Calculate total waiting time and total turn
+		// around time
+		for (int i = 0; i < n; i++) {
+			total_wt = total_wt + wt[i];
+			total_tat = total_tat + tat[i];
+			System.out.printf(" %d ", (i + 1));
+			System.out.printf("	 %d ", bt[i]);
+			System.out.printf("	 %d", wt[i]);
+			System.out.printf("	 %d\n", tat[i]);
+		}
+		float s = (float)total_wt /(float) n;
+		int t = total_tat / n;
+		System.out.printf("Average waiting time = %f", s);
+		System.out.printf("\n");
+		System.out.printf("Average turn around time = %d ", t);
+	}
+
+	// Driver code
+	public static void main(String[] args) throws ParseException {
+		//process id's
+		int processes[] = {1, 2, 3};
+		int n = processes.length;
+
+		//Burst time of all processes
+		int burst_time[] = {10, 5, 8};
+
+		findavgTime(processes, n, burst_time);
+
+	}
 }
-
-// Driver code
-int main()
-{
-	//process id's
-	int processes[] = { 1, 2, 3};
-	int n = sizeof processes / sizeof processes[0];
-
-	//Burst time of all processes
-	int burst_time[] = {10, 5, 8};
-
-	findavgTime(processes, n, burst_time);
-	return 0;
-}
-// This code is contributed by Shivi_Aggarwal
+// This code is contributed by 29ajaykumar
