5cb673e4787bda5a529f2cc4c38df04180b52ef3
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-13 23:43:47 +0530

    day-15

---
+++ CheckPrime.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int flag = 0;
+    for(int i =2;i<=sqrt(n);i++){
+        if(n%i==0){
+            flag = 1;
+            break;
+        }       
+    }
+    if(flag==0) cout<<"Prime Number"<<endl;
+    else cout<<"Non prime"<<endl;
+    return 0;
+}
