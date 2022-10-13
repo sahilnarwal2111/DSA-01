f60915f2fb59ab0671fe1ff9e06c6e91ccec6926
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-13 23:45:17 +0530

    day-15

---
+++ PalindromicPattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i = 1; i<=n;i++){
+        for(int j = n-i;j>=0;j--){
+            cout<<"  ";
+        }
+        int count = i;
+        for(int k = 1; k<=(2*i-1);k++){
+            cout<<count<<" ";
+            if(count>1) count--;
+            else count++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
