f9d8780901dc5d9a28bd9877ff6881c70403f203
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-13 23:43:06 +0530

    day-15

---
+++ Armstrong.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int sum = 0;
+    int originaln = n;
+    while(n>0){
+        int digit = n%10;
+        sum+= pow(digit,3);
+        n/=10;
+        // cout<<digit<<endl;
+    }
+    if(sum==originaln) cout<<"Amrstrong Number"<<endl;
+    else cout<<"Non Armstrong Number"<<endl;
+    return 0;
+}
