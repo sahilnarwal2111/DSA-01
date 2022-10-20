65d4017c9fe59a23eb57ffdea4d4e07cfad8d10e
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2022-10-20 23:29:47 +0530

    Update PalindromicPattern.cpp

--- PalindromicPattern.cpp
+++ PalindromicPattern.cpp
@@ -8,6 +8,7 @@ int main()
     for(int i = 1; i<=n;i++){
         for(int j = n-i;j>=0;j--){
             cout<<"  ";
+            
         }
         int count = i;
         for(int k = 1; k<=(2*i-1);k++){
